57f80dc44667d566325d900e7de4b16a83dd32da
Franco Monroy <jdcyonel2@gmail.com>
2023-10-03 08:03:08 -0500

    Se crearon las demas vistas

---
+++ resources/views/Login.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="ContentForm">
+            <h1>Login</h1>
+            <form method="POST">
+                <label>
+                    <text>UserName</text>
+                    <input type="text" width=120> 
+                </label>    
+                <label>
+                    <text>Password</text>
+                    <input type="text" width=120> 
+                </label>   
+                <div>
+                    <text>No tiene una cuenta?
+                    <a href="<?=route('register')?>">Registrate Aquí</a>
+                    </text>  
+                </div>
+                <button type="Submit">Login</button>
+            </form>
+        </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/dashboard','Dashboard');
+
+Route::view('/register','Register')->name('register');
+
+Route::view('/login','Login');
